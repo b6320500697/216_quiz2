7729e773f2e0e32cd6d9dd5ec38c70a2b4aa6dc2
b6320500697 <78530335+b6320500697@users.noreply.github.com>
2021-03-25 17:11:39 +0700

    ข้อ1

--- 6320500697-1.c
+++ 6320500697-1.c
@@ -1,5 +1,16 @@
 #include <stdio.h>
 int main()
 {
+    int number,i,k;
+    scanf ("%d",&number);
 
+    for(i=2; ;i++)
+    {
+        if(number==pow(i,3))
+        {
+            printf ("Not Cube Free");
+            break;
+        }
+    }
+    return 0;
 }
