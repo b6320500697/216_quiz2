a226128a4d131520f64bf9751344ac0c4f1366e0
b6320500697 <78530335+b6320500697@users.noreply.github.com>
2021-03-25 17:28:27 +0700

    ข้อ3

--- 6320500697-3.c
+++ 6320500697-3.c
@@ -1,5 +1,23 @@
 #include <stdio.h>
 int main()
 {
+    int n,i,k,j;
+    scanf ("%d",&n);
+    i=9;
+    k=9;
+    j=90;
+    if(n<=10)
+    {
+        printf ("%d",i);
+    }
+    else if (n<=100)
+    {
+        printf ("%d",i+k);
+    }
+    else
+    {
+        printf ("%d",i+k+j);
+    }
+    return 0;
 
 }
